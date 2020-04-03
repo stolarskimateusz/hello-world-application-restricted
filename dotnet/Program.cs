1f8bde6542044976cdd8b2749cb93b275e4b19a6
Mateusz Stolarski <mateusz.stolarski@airelogic.com>
2020-04-03 12:01:37 +0100

    [DOTNET] remove redundant requirements

--- dotnet/Program.cs
+++ dotnet/Program.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Net.Http;
-using System.Net.Http.Headers;
 
 
 namespace dotnetapi
@@ -25,8 +23,6 @@ namespace dotnetapi
             var responseString = response.Content.ReadAsStringAsync().Result;
             
             Console.WriteLine(responseString);
-
-            
         }
     }
 }
